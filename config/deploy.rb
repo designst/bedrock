7f64b2cf21b727941cee0422a81137ebad50f097
designst <info@designstage.ch>
2014-11-08 15:29:30 +0100

    style(tasks): remove unused spaces

--- config/deploy.rb
+++ config/deploy.rb
@@ -69,24 +69,24 @@ namespace :deploy do
     end
 
     desc 'Update WordPress template root paths to point to the new release'
-        task :update_option_paths do
-            on roles(:app) do
-                within fetch(:release_path) do
-                    if test :wp, :core, 'is-installed'
-                        [:stylesheet_root, :template_root].each do |option|
-                            # Only change the value if it's an absolute path
-                            # i.e. The relative path "/themes" must remain unchanged
-                            # Also, the option might not be set, in which case we leave it like that
-                            value = capture :wp, :option, :get, option, raise_on_non_zero_exit: false
+    task :update_option_paths do
+        on roles(:app) do
+            within fetch(:release_path) do
+                if test :wp, :core, 'is-installed'
+                    [:stylesheet_root, :template_root].each do |option|
+                        # Only change the value if it's an absolute path
+                        # i.e. The relative path "/themes" must remain unchanged
+                        # Also, the option might not be set, in which case we leave it like that
+                        value = capture :wp, :option, :get, option, raise_on_non_zero_exit: false
 
-                            if value != '' and value != '/themes'
-                                execute :wp, :option, :set, option, fetch(:release_path).join('web/wp/wp-content/themes')
-                            end
+                        if value != '' and value != '/themes'
+                            execute :wp, :option, :set, option, fetch(:release_path).join('web/wp/wp-content/themes')
                         end
                     end
                 end
             end
         end
+    end
 end
 
 after 'deploy:started', 'deploy:composer'
