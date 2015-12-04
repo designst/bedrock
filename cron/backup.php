0b34ab61bc1854ea84f109090a5bdd39b7642b75
designst <repo@designstage.ch>
2015-12-04 08:26:14 +0100

    feat(cron): add mysql backup script

---
+++ cron/backup.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once( dirname( __DIR__ ) . '/vendor/autoload.php' );
+
+$root_dir = dirname( __DIR__ );
+$dotenv = new Dotenv\Dotenv( $root_dir );
+
+if ( file_exists( $root_dir . '/.env' ) ) {
+	$dotenv->load();
+	$dotenv->required( ['DB_NAME', 'DB_USER', 'DB_PASSWORD', 'DB_HOST'] );
+}
+
+$db_user = getenv( 'DB_USER' );
+$db_password = getenv( 'DB_PASSWORD' );
+$db_host = getenv( 'DB_HOST' );
+$db_name = getenv( 'DB_NAME' );
+
+$backup_path = $root_dir . '/db/';
+$backup_name = $backup_path . $db_name . '_' . date( 'Y-m-d-His' ) . '.sql.bz2';
+
+exec( "mysqldump --user=$db_user --password=$db_password --host=$db_host $db_name --lock-tables=false | bzip2 -9 > $backup_name" );
